d5455c3d8dcfa70086c7569671044b9ae05ee6d8
Stefano Vannucchi <stefano.vannucchi1@gmail.com>
2020-05-19 08:43:12 +0200

    init

---
+++ src/main/java/it/polste/attsw/teammatesmanagerbackend/TeammatesManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package it.polste.attsw.teammatesmanagerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeammatesManagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeammatesManagerBackendApplication.class, args);
+	}
+
+}
