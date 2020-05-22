1e0e271f0e3afbb98cd9bb532554d3971b46dd06
Pol52 <innocentipaolo1@gmail.com>
2020-05-22 13:24:17 +0200

    added mock test to verify jenkins correct deployment

---
+++ src/test/java/it/polste/attsw/teammatesmanagerbackend/TeammatesManagerBackendApplicationTest.java
@@ -0,0 +1,18 @@
+package it.polste.attsw.teammatesmanagerbackend;
+
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TeammatesManagerBackendApplicationTest {
+
+    public TeammatesManagerBackendApplicationTest(){
+
+    }
+
+    @Test
+    public void mockTestForJenkinsSetup(){
+        boolean working = true;
+        assertTrue(working);
+    }
+}
