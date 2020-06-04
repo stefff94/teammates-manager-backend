d1caae0ed1fff19353ba4331bed818259213899b
Stefano Vannucchi <stefano.vannucchi1@gmail.com>
2020-06-04 17:35:38 +0200

    custom assertions on skill jpa test

--- src/test/java/it/polste/attsw/teammatesmanagerbackend/SkillJpaTest.java
+++ src/test/java/it/polste/attsw/teammatesmanagerbackend/SkillJpaTest.java
@@ -3,15 +3,20 @@ package it.polste.attsw.teammatesmanagerbackend;
 import it.polste.attsw.teammatesmanagerbackend.models.Skill;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @RunWith(SpringRunner.class)
 public class SkillJpaTest {
+
+  private static final Logger logger = LoggerFactory.getLogger(SkillJpaTest.class);
 
   @Autowired
   private TestEntityManager entityManager;
@@ -20,6 +25,12 @@ public class SkillJpaTest {
   public void testJpaMapping() {
     Skill skill =
             entityManager.persistFlushFind(new Skill(null, "Spring Boot"));
+
+    assertThat(skill.getName()).isEqualTo("Spring Boot");
+    assertThat(skill.getId()).isNotNull();
+    assertThat(skill.getId()).isGreaterThan(0);
+
+    logger.info("Persisted entity with id: " + skill.getId());
   }
 
 }
