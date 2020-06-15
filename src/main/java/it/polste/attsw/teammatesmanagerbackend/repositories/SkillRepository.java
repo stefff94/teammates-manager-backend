d66a1a00243259a00ee5b4474bb4b40d8db8e665
Pol52 <innocentipaolo1@gmail.com>
2020-06-15 10:04:26 +0200

    added fake implementation for SkillRepository Added

--- src/main/java/it/polste/attsw/teammatesmanagerbackend/repositories/SkillRepository.java
+++ src/main/java/it/polste/attsw/teammatesmanagerbackend/repositories/SkillRepository.java
@@ -1,4 +1,25 @@
 package it.polste.attsw.teammatesmanagerbackend.repositories;
 
+import it.polste.attsw.teammatesmanagerbackend.models.Skill;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public class SkillRepository {
+
+    private static final String TEMPORARY_IMPLEMENTATION = "Temporary Implementation";
+
+    public List<Skill> findAll(){
+        throw new UnsupportedOperationException(TEMPORARY_IMPLEMENTATION);
+    }
+
+    public Optional<Skill> findById(long id){
+        throw new UnsupportedOperationException(TEMPORARY_IMPLEMENTATION);
+    }
+
+    public Skill save(Skill skill){
+        throw new UnsupportedOperationException(TEMPORARY_IMPLEMENTATION);
+    }
 }
