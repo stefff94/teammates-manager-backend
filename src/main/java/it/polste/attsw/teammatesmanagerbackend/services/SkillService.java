821f0a305bbe47dd9983190df0b644b4463127f4
Pol52 <innocentipaolo1@gmail.com>
2020-06-15 10:29:41 +0200

    create getAllSkills method for SkillService

--- src/main/java/it/polste/attsw/teammatesmanagerbackend/services/SkillService.java
+++ src/main/java/it/polste/attsw/teammatesmanagerbackend/services/SkillService.java
@@ -1,4 +1,23 @@
 package it.polste.attsw.teammatesmanagerbackend.services;
 
+import it.polste.attsw.teammatesmanagerbackend.models.Skill;
+import it.polste.attsw.teammatesmanagerbackend.repositories.SkillRepository;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class SkillService {
+
+    private final SkillRepository skillRepository;
+
+    public SkillService(SkillRepository skillRepository){
+        this.skillRepository = skillRepository;
+    }
+
+    public List<Skill> getAllSkills(){
+        return skillRepository.findAll();
+    }
 }
