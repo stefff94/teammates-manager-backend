07e90660b4b8b38bfeaf561eea6ecc762b4394d7
Stefano Vannucchi <stefano.vannucchi1@gmail.com>
2020-06-17 12:26:45 +0200

    refactored TeammateRestControllerTest using rest-assured

--- src/test/java/it/polste/attsw/teammatesmanagerbackend/controllers/TeammateRestControllerTest.java
+++ src/test/java/it/polste/attsw/teammatesmanagerbackend/controllers/TeammateRestControllerTest.java
@@ -1,44 +1,50 @@
 package it.polste.attsw.teammatesmanagerbackend.controllers;
 
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import it.polste.attsw.teammatesmanagerbackend.models.PersonalData;
 import it.polste.attsw.teammatesmanagerbackend.models.Teammate;
 import it.polste.attsw.teammatesmanagerbackend.services.TeammateService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.Arrays;
 
-import static org.hamcrest.CoreMatchers.*;
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.when;
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.Matchers.empty;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@RunWith(SpringRunner.class)
-@WebMvcTest(controllers = TeammateRestController.class)
+@RunWith(MockitoJUnitRunner.class)
 public class TeammateRestControllerTest {
 
-  @Autowired
-  private MockMvc mockMvc;
-
-  @MockBean
+  @Mock
   private TeammateService teammateService;
 
-  @Test
-  public void testGetAllTeammatesWhenEmpty() throws Exception {
-    this.mockMvc.perform(get("/api/teammates")
-            .accept(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk())
-            .andExpect(content().json("[]"));
+  @InjectMocks
+  private TeammateRestController teammateRestController;
+
+  @Before
+  public void setup() {
+    RestAssuredMockMvc.standaloneSetup(teammateRestController);
   }
 
   @Test
-  public void testGetAllTeammatesWhenNotEmpty() throws Exception {
+  public void testGetAllTeammatesWhenEmpty() {
+    when().
+            get("/api/teammates").
+    then().
+            statusCode(200).
+            assertThat().
+            body("", empty());
+  }
+
+  @Test
+  public void testGetAllTeammatesWhenNotEmpty() {
     PersonalData stefanosData = new PersonalData("Stefano Vannucchi",
             "stefano.vannucchi@stud.unifi.it",
             "M",
@@ -59,25 +65,26 @@ public class TeammateRestControllerTest {
                     new Teammate(2L, paolosData)
             ));
 
-    this.mockMvc.perform(get("/api/teammates"))
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("$[0].id", is(1)))
-            .andExpect(jsonPath("$[0].personalData.name", is("Stefano Vannucchi")))
-            .andExpect(jsonPath("$[0].personalData.email",
-                            is("stefano.vannucchi@stud.unifi.it")))
-            .andExpect(jsonPath("$[0].personalData.gender", is("M")))
-            .andExpect(jsonPath("$[0].personalData.city", is("Prato")))
-            .andExpect(jsonPath("$[0].personalData.role", is("student")))
-            .andExpect(jsonPath("$[0].personalData.photoUrl",
-                    is("https://semantic-ui.com/images/avatar/large/steve.jpg")))
-            .andExpect(jsonPath("$[1].id", is(2)))
-            .andExpect(jsonPath("$[1].personalData.email",
-                    is("paolo.innocenti@stud.unifi.it")))
-            .andExpect(jsonPath("$[1].personalData.gender", is("M")))
-            .andExpect(jsonPath("$[1].personalData.city", is("Pistoia")))
-            .andExpect(jsonPath("$[1].personalData.role", is("student")))
-            .andExpect(jsonPath("$[1].personalData.photoUrl",
-                    is("https://semantic-ui.com/images/avatar/large/elliot.jpg")));
+    when().
+            get("/api/teammates").
+    then().
+            statusCode(200).
+            assertThat().
+            body(
+                    "id", hasItems(1, 2),
+                    "personalData.name", hasItems("Stefano Vannucchi", "Paolo Innocenti"),
+                    "personalData.email", hasItems(
+                            "stefano.vannucchi@stud.unifi.it",
+                            "paolo.innocenti@stud.unifi.it"
+                    ),
+                    "personalData.gender", hasItem("M"),
+                    "personalData.city", hasItems("Prato", "Pistoia"),
+                    "personalData.role", hasItem("student"),
+                    "personalData.photoUrl", hasItems(
+                            "https://semantic-ui.com/images/avatar/large/steve.jpg",
+                            "https://semantic-ui.com/images/avatar/large/elliot.jpg"
+                    )
+            );
   }
 
 }
