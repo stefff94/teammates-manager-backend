3614c9227b9798145baec1e61af3f651d326b802
Stefano Vannucchi <stefano.vannucchi1@gmail.com>
2020-06-30 09:33:25 +0200

    added cors policy

---
+++ src/main/java/it/polste/attsw/teammatesmanagerbackend/WebConfig.java
@@ -0,0 +1,19 @@
+package it.polste.attsw.teammatesmanagerbackend;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/api/**")
+            .allowedOrigins("*")
+            .allowedMethods("*")
+            .allowedHeaders("*");
+  }
+}
